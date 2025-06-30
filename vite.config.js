03c60bf46d98e8c85c8c5e3199cdae24cd9ee9ce
TonNom <ton.email@example.com>
2025-06-30 16:27:16 +0200

    Sauvegarde avant modification du projet

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/projet-final-react/', // Important pour GitHub Pages
   plugins: [react()],
