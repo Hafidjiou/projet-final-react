6517eb2af2310c80fda13798d3c4a8ea157ffb64
TonNom <ton.email@example.com>
2025-06-30 15:39:51 +0200

    Ajoute base pour GitHub Pages dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
+  base: '/projet-final-react/', // Important pour GitHub Pages
   plugins: [react()],
   server: {
     port: 5174
